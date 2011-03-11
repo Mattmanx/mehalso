29ee525d57069910612eee11ad37e2fc911ad39c
Matt Mehalso <mehalso@gmail.com>
2011-03-11 07:43:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MehalsoCom::Application.config.secret_token = 'e3584c0a74076e5d8ef23c3d6e46b56e48a27b3883eb9d3eaa6f6c0fa490f6bea689db9d9b3a1b27bd472f15620bbb6a71ab9ceaa49b0ddf69fa69a1cd50ce66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MehalsoCom::Application.config.session_store :cookie_store, :key => '_mehalso.com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MehalsoCom::Application.config.session_store :active_record_store
