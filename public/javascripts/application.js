39b5995aaae3c2750eb2a05796dea70a0b908059
Matt Mehalso <mehalso@gmail.com>
2011-03-12 21:33:48 -0500

    Adding stylesheets, updating application.html.erb

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,27 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+
+    var theWindow        = $(window),
+        $bg              = $("#bg"),
+        aspectRatio      = $bg.width() / $bg.height();
+
+    function resizeBg() {
+
+            if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+                $bg
+                    .removeClass()
+                    .addClass('bgheight');
+            } else {
+                $bg
+                    .removeClass()
+                    .addClass('bgwidth');
+            }
+
+    }
+
+    theWindow.resize(function() {
+            resizeBg();
+    }).trigger("resize");
+
+});
